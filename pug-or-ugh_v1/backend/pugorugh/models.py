6b707acc7ba472ca3a39a9ae83b93f396220e276
byCM <43051181+byCM@users.noreply.github.com>
2019-02-12 19:17:10 -0900

    Update models.py

--- pug-or-ugh_v1/backend/pugorugh/models.py
+++ pug-or-ugh_v1/backend/pugorugh/models.py
@@ -58,13 +58,13 @@ class UserPref(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserPref.objects.create(user=instance)
+      UserPref.objects.create(user=instance)
 
-    dogs = Dog.objects.all()
+      dogs = Dog.objects.all()
 
-    for dog in dogs:
-        UserDog.objects.create(
-            user=instance,
-            dog=dog,
-            status='u',
-        )
+      for dog in dogs:
+          UserDog.objects.create(
+              user=instance,
+              dog=dog,
+              status='u',
+          )
