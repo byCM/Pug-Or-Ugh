2c44738c4023f24b28f076dd2d82a20a9a74b2f6
byCM <43051181+byCM@users.noreply.github.com>
2019-02-20 11:45:13 -0900

    Update urls.py

--- pug-or-ugh_v1/backend/pugorugh/urls.py
+++ pug-or-ugh_v1/backend/pugorugh/urls.py
@@ -12,8 +12,8 @@ urlpatterns = format_suffix_patterns([
     url(r'^api/user/login/$', obtain_auth_token, name='login-user'),
     url(r'^api/user/$', UserRegisterView.as_view(), name='register-user'),
     url(r'^api/user/preferences/$', UserPrefUpdateView.as_view(), name='update-prefs'),
-    url(r'^api/dog/(?P<pk>\d+)/(?P<type>liked|disliked|undecided)/$', UpdateDogStatusView.as_view(), name='update-dog'),
-    url(r'^api/dog/(?P<pk>-?\d+)/(?P<type>liked|disliked|undecided)/next/$', RetrieveNextDogView.as_view(), name='next-dog'),
+    url(r'^api/dog/(?P<pk>\d+)/(?P<type>liked|disliked|undecided)/$', , name='update-dog'),
+    url(r'^api/dog/(?P<pk>-?\d+)/(?P<type>liked|disliked|undecided)/next/$', , name='next-dog'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/icons/favicon.ico', permanent=True)),
     url(r'^$', TemplateView.as_view(template_name='index.html'))
 ])
