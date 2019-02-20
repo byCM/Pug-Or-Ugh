7eb7d2ca0d8d39f682a3c9eef00bf89a28b0f058
byCM <43051181+byCM@users.noreply.github.com>
2019-02-20 11:45:24 -0900

    Update urls.py

--- pug-or-ugh_v1/backend/pugorugh/urls.py
+++ pug-or-ugh_v1/backend/pugorugh/urls.py
@@ -5,15 +5,14 @@ from django.views.generic.base import RedirectView
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import (UserRegisterView, retrieve_next_dog)
+from .views import (UserRegisterView, retrieve_next_dog, update_dog)
 
 # API endpoints
 urlpatterns = format_suffix_patterns([
     url(r'^api/user/login/$', obtain_auth_token, name='login-user'),
     url(r'^api/user/$', UserRegisterView.as_view(), name='register-user'),
-    url(r'^api/user/preferences/$', UserPrefUpdateView.as_view(), name='update-prefs'),
-    url(r'^api/dog/(?P<pk>\d+)/(?P<type>liked|disliked|undecided)/$', , name='update-dog'),
-    url(r'^api/dog/(?P<pk>-?\d+)/(?P<type>liked|disliked|undecided)/next/$', , name='next-dog'),
+    url(r'^api/dog/(?P<pk>-?\d+)/(?P<type>liked|disliked|undecided)/$', update_dog, name='update-dog'),
+    url(r'^api/dog/(?P<pk>-?\d+)/(?P<type>liked|disliked|undecided)/next/$', retrieve_next_dog, name='next-dog'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/icons/favicon.ico', permanent=True)),
     url(r'^$', TemplateView.as_view(template_name='index.html'))
 ])
